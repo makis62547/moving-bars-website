f6f1b014f783876259d14494588b6ab9b5ace135
makis62547 <markus.weilnboeck@me.com>
2025-12-15 19:29:54 +0100

    Add simple i18n support for hero and navigation

---
+++ src/i18n/de.ts
@@ -0,0 +1,12 @@
+export const de = {
+  nav: {
+    cocktail: "Cocktail Catering",
+    mobileBar: "Mobile Bar",
+    coffee: "Kaffee Catering",
+    request: "Anfrage",
+  },
+  hero: {
+    title: "Mobile Cocktailbar mieten in Berlin",
+    intro: "Professionelles Cocktail Catering für Events, Firmenfeiern & Hochzeiten.",
+  },
+};
---
+++ src/i18n/index.ts
@@ -0,0 +1,5 @@
+import { de } from "./de";
+import { en } from "./en";
+
+export type Lang = "de" | "en";
+export const i18n = { de, en };
